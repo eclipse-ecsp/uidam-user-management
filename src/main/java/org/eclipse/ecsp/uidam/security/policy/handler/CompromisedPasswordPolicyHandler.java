72475bf17f20221b0e406c9e3b770c846323697b
puvvada88 <venkatakumar.puvvada@harman.com>
2025-06-17 18:32:50 +0530

    Added more testcases to meet the coverage

--- src/main/java/org/eclipse/ecsp/uidam/security/policy/handler/CompromisedPasswordPolicyHandler.java
+++ src/main/java/org/eclipse/ecsp/uidam/security/policy/handler/CompromisedPasswordPolicyHandler.java
@@ -19,7 +19,6 @@
 package org.eclipse.ecsp.uidam.security.policy.handler;
 
 import org.apache.commons.lang3.StringUtils;
-import org.eclipse.ecsp.uidam.security.policy.handler.PasswordValidationInput;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
